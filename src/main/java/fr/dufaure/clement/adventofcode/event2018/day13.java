e4431c584930ba0000f78b78ed18963d8e2be70c
Clement Dufaure <clementdufaure@hotmail.com>
2018-12-13 08:41:17 +0100

    doing day13

---
+++ src/main/java/fr/dufaure/clement/adventofcode/event2018/day13.java
@@ -0,0 +1,66 @@
+package fr.dufaure.clement.adventofcode.event2018;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import fr.dufaure.clement.adventofcode.utils.ImportUtils;
+
+public class day13 {
+	public static void main(String[] args) {
+		long start1 = System.currentTimeMillis();
+		part1();
+		System.out.println("Execution part 1 en " + (System.currentTimeMillis() - start1) + " ms");
+	}
+
+	static List<List<Rail>> reseau;
+
+	static {
+		// creation du reseau
+		reseau = ImportUtils.getListStringUnParLigne("./src/main/resources/2018/day13").stream().map(day13::parseLigne)
+				.collect(Collectors.toList());
+	}
+
+	static List<Rail> parseLigne(String ligne) {
+		List<Rail> rails = new ArrayList<>();
+		// on s'interesse au reseau :remplacement des wagonnets par des rails
+		// correspondants
+		ligne = ligne.replaceAll("[<>]", "-");
+		ligne = ligne.replaceAll("[v^]", "|");
+		for (char c : ligne.toCharArray()) {
+			rails.add(Rail.getTypeRailFromChar(c));
+		}
+		return rails;
+	}
+
+	static void part1() {
+
+	}
+
+	public static enum Rail {
+		HORIZONTAL('|'), VERTICAL('-'), DIAGONAL('/'), DIAGONALBACK('\\'), CROISEMENT('+'), RIEN(' ');
+
+		private char charactere;
+
+		private Rail(char c) {
+			charactere = c;
+		}
+
+		public static Rail getTypeRailFromChar(char c) {
+			for (Rail r : Rail.values()) {
+				if (r.charactere == c) {
+					return r;
+				}
+			}
+			System.err.println("problem");
+			return Rail.RIEN;
+		}
+	}
+	
+	public static class Wagonnet{
+		int x;
+		int y;
+		Orientation orientation;
+	}
+
+}
